148ad1d92f48264233cc918c5ad83d8ccbd26cb5
Silvio Ney <sneycampos@gmail.com>
2024-09-11 21:37:31 +0100

    Added 2Fast 2Furious

--- shrek.js
+++ shrek.js
@@ -1,24 +1,24 @@
-async function enviarScript(scriptText){
+async function sendScript(scriptText){
 	const lines = scriptText.split(/[\n\t]+/).map(line => line.trim()).filter(line => line);
 	main = document.querySelector("#main"),
-	textarea = main.querySelector(`div[contenteditable="true"]`)
-	
-	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
+		textarea = main.querySelector(`div[contenteditable="true"]`)
+
+	if(!textarea) throw new Error("There is no conversation open")
+
 	for(const line of lines){
 		console.log(line)
-	
+
 		textarea.focus();
 		document.execCommand('insertText', false, line);
 		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
+
 		setTimeout(() => {
 			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
 		}, 100);
-		
+
 		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
 	}
-	
+
 	return lines.length;
 }
 
